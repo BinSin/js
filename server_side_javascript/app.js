f35c16b57ec961ba0e63cb77f8c6cef09d80bcca
BinSin <binyong318@gmail.com>
2018-03-20 20:15:57 +0900

    18/3/19

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -1,11 +1,71 @@
 var express = require('express');
 var app = express();
+
+app.locals.pretty = true; // jade 코드 이쁘게
+app.set('view engine', 'pug');
+app.set('views', './views'); // template 파일을 views 디렉토리에 저장
+
+app.use(express.static('public')); // 정적인 파일 서비스
+
+app.get('/topic/:id', function(req, res) {
+  var topics = [
+    'Javascript is ...',
+    'Nodejs is ...',
+    'Express is ...',
+  ];
+  var output = `
+    <a href="/topic?id=0">JavaScript</a><br>
+    <a href="/topic?id=1">Nodejs</a><br>
+    <a href="/topic?id=2">Express</a><br>
+    ${topics[req.params.id]}
+  `
+  res.send(output);
+});
+
+app.get('/topic/:id/:node', function(req, res){
+  res.send(req.params.id + ', ' + req.params.node);
+});
+
+app.get('/template', function(req, res) {
+  res.render('temp', {time: Date(), title: 'Pug'});
+}); // template경로를 통해 들어온 사용자에게 웹페이지에 temp파일을 렌더링해서 전송한다.
+
 app.get('/', function(req, res) { // req(요청), res(응답) 약속!!
   res.send('Hello home page');
 }); // 사용자가 /(home)으로 접속하면 function함수가 발생함
+app.get('/dynamic', function(req, res) {
+  var lis = ' ';
+  for(var i=0; i<5; i++) {
+    lis = lis + '<li>coding</li>';
+  }
+  var time = Date();
+  var output = `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      Hello, Dynamic!
+      <ul>
+        ${lis}
+      </ul>
+      ${time}
+    </body>
+  </html>`;
+  res.send(output);
+  // \로 여러줄을 쓸 수 있다. 그것보다 `가 더 편하다
+  // ${변수}
+});
+
+app.get('/route',  function(req, res) {
+  res.send('Hello Router, <img src="/route.png">');
+});
+
 app.get('/login', function(req, res) { // get은 연결해주는 함수
   res.send('<h1>Login please</h1>');
-})
+});
+
 app.listen(2999, function() {
   console.log('Connected 2999 port!');
 });
